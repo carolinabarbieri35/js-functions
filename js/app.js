e375e0b4e715aa3ae56daaa446fd98c421a6245c
Carolina Barbieri <carolinabarbieri35@gmail.com>
2026-08-22 07:27:51 +0200

    esercizio.2

--- js/app.js
+++ js/app.js
@@ -20,4 +20,24 @@ const dollars = converter(500);
 console.log('La tua somma convertita in $ è di', dollars);
 
 
+// Un taxi applica una tariffa extra fissa dopo la mezzanotte. Scrivi una funzione che calcoli il totale della corsa dato il prezzo base all'orario di partenza e all'orario di arrivo.
+
+/**
+ * 
+ * @param {number} prezzoPartenza 
+ * @param {number} prezzoArrivo 
+ */
+function totaleCorsa(prezzoPartenza, prezzoArrivo) {
+
+    const prezzo = prezzoPartenza + prezzoArrivo;
+
+    const extra = prezzo + (prezzo * 25 / 100);
+
+    return extra;
+
+
+}
+
+const corsa = totaleCorsa(26, 32);
+console.log('il totale della corsa è', corsa, '€');
 
